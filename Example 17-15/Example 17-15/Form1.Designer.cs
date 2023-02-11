29e93a4d91c6c117c663be3ba9f688c9908e1563
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 15:19:05 +0330

    Add files via upload

---
+++ Example 17-15/Example 17-15/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Example_17_15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImport = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(12, 17);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(162, 40);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "ورود تصویر";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 63);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(502, 402);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(352, 17);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(162, 40);
+            this.btnNewGame.TabIndex = 1;
+            this.btnNewGame.Text = "بهم ریختن تصویر";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 477);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnImport);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Example 17-15 (Joorchin Game)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnNewGame;
+    }
+}
+
---
+++ Example 17-15/Example 17-15/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example_17_15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //-------------------------------------------------------------------
+        Button[] AllImages = new Button[16];
+        Point[] DefaultLocation = new Point[16];
+        //-------------------------------------------------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = this.MinimizeBox = false;
+            for (int i = 0; i <= 15; i++)
+            {
+                AllImages[i] = new Button();
+                int x = (i % 4) * 125;
+                int y = i / 4 * 100;
+                AllImages[i].Location = new Point(x, y);
+                AllImages[i].Size = new Size(125,100);
+                AllImages[i].Tag = i;
+                AllImages[i].BackgroundImageLayout = ImageLayout.Zoom;
+                AllImages[i].Click += new EventHandler(AllImage_Click);
+                panel1.Controls.Add(AllImages[i]);
+                DefaultLocation[i] = AllImages[i].Location;
+            }
+        }
+        //-------------------------------------------------------------------
+        int jabejayi = -1;
+        //-------------------------------------------------------------------
+        private void AllImage_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            switch (jabejayi)
+            {
+                case -1:
+                    if (b.FlatStyle == FlatStyle.Standard)
+                    {
+                        b.FlatStyle = FlatStyle.Flat;
+                        jabejayi = Convert.ToInt32(b.Tag);
+                    }
+                    else
+                    {
+                        b.FlatStyle = FlatStyle.Standard;
+                        jabejayi = -1;
+                    }
+                    break;
+                default:
+                    Point temp = AllImages[jabejayi].Location;
+                    AllImages[jabejayi].Location = b.Location;
+                    b.Location = temp;
+                    AllImages[jabejayi].FlatStyle = FlatStyle.Standard;
+                    jabejayi = -1;
+                    Winner();
+                    break;
+            }            
+        }
+        //-------------------------------------------------------------------
+        private void Winner()
+        {
+            for (int i = 0; i <= 15; i++)
+                if (DefaultLocation[i] != AllImages[i].Location)
+                    return;
+            MessageBox.Show("!You Win!");
+        }
+        //-------------------------------------------------------------------
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Jpeg Files (*.jpg)|*.jpg|Bitmap Files (*.bmp)|*.bmp";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.SafeFileName != "")
+            {
+                Bitmap mainPicture = new Bitmap(Image.FromFile(openFileDialog1.FileName), 500, 400);
+                for (int i = 0; i <= 15; i++)
+                {
+                    int x = (i % 4) * 125;
+                    int y = i / 4 * 100;
+                    AllImages[i].Location = new Point(x, y);
+                    Rectangle rectPortion = new Rectangle(AllImages[i].Left, AllImages[i].Top, 125, 100);
+                    Bitmap get_part = new Bitmap(125, 100);
+                    get_part = mainPicture.Clone(rectPortion, System.Drawing.Imaging.PixelFormat.Undefined);
+                    AllImages[i].BackgroundImage = get_part;
+                }              
+            }
+        }
+        //-------------------------------------------------------------------
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            //بهم زدن ترتیب تصویرها
+            Random R = new Random();
+            for (int i = 1; i <= R.Next(10, 20); i++)//احتمال جابجایی حداقل 10 و حداکثر 19 بار
+            {
+                int first, next;
+                first = R.Next(0, 16);//[0,15]
+                do
+                {
+                    next = R.Next(0, 16);
+                } while (next == first);
+                Point temp = AllImages[first].Location;
+                AllImages[first].Location = AllImages[next].Location;
+                AllImages[next].Location = temp;
+            }
+        }
+    }
+}
